89ee21c737e00999378c0a1394728c1bfe0abf7f
Andrew Collington <andy@amnuts.com>
2020-04-19 01:24:26 +0100

    Changed '0' to '?' to indicate unknown number.

--- contentScript.js
+++ contentScript.js
@@ -7,10 +7,10 @@ let applyCommentIcon = (file) => {
         let comments = file.querySelectorAll('tr.inline-comments').length;
         let insertNode = file.querySelector('div.file-actions > div');
         insertNode.innerHTML = `
-            <span class="comment-count ml-1 mr-2 text-normal d-flex flex-items-center" title="Comments on this file: ${comments}">
+            <span class="comment-count ml-1 mr-2 text-normal d-flex flex-items-center" title="Comments on this file: ${comments == 0 ? 'unknown' : comments}">
                 <svg viewBox="0 0 24 24" height="24" width="24" xmlns="http://www.w3.org/2000/svg">
                     <path d="M24 1h-24v16.981h4v5.019l7-5.019h13z" fill="grey"></path>
-                    <text x="50%" y="45%" dominant-baseline="middle" text-anchor="middle" fill="white">${comments}</text>
+                    <text x="50%" y="45%" dominant-baseline="middle" text-anchor="middle" fill="white">${comments == 0 ? '?' : comments}</text>
                 </svg>
             </span>
         ` + insertNode.innerHTML;
