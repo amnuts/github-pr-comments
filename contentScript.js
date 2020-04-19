4da4218d0b93c6ce610d12765535ed9d434b187b
Andrew Collington <andy@amnuts.com>
2020-04-19 01:22:00 +0100

    Had things apply a bit better when refresh from single page app or full reload.
    The observer now attaches to a much higher node (the body!) and things worked out from there.
    The non-progressive file list loaded is not targeted more specifically.
    Now if a file has comments but the content was too big to load, show a '0' speech bubble to indicate that there are comments there but until the content is loaded we don't know how many.

--- contentScript.js
+++ contentScript.js
@@ -5,9 +5,8 @@ let applyCommentIcon = (file) => {
     }
     if (file.classList.contains('has-inline-notes')) {
         let comments = file.querySelectorAll('tr.inline-comments').length;
-        if (comments) {
-            let insertNode = file.querySelector('div.file-actions > div');
-            insertNode.innerHTML = `
+        let insertNode = file.querySelector('div.file-actions > div');
+        insertNode.innerHTML = `
             <span class="comment-count ml-1 mr-2 text-normal d-flex flex-items-center" title="Comments on this file: ${comments}">
                 <svg viewBox="0 0 24 24" height="24" width="24" xmlns="http://www.w3.org/2000/svg">
                     <path d="M24 1h-24v16.981h4v5.019l7-5.019h13z" fill="grey"></path>
@@ -15,7 +14,6 @@ let applyCommentIcon = (file) => {
                 </svg>
             </span>
         ` + insertNode.innerHTML;
-        }
     }
 };
 
@@ -31,6 +29,12 @@ let mutateCallback = (mutationsList, observer) => {
             newFiles.forEach(file => {
                 applyCommentIcon(file);
             });
+            // this is to work around the SPA loading portions of the file
+            // list again but this plugin not really picking up well on the
+            // pre-rendered files
+            if (newFiles.length) {
+                initApply(document.getElementById('files'));
+            }
             // if adding or removing an inline comment in a single file
             // or loading a diff that was too large to initially load
             if (changedNodes.some(node => {
@@ -48,7 +52,7 @@ let mutateCallback = (mutationsList, observer) => {
 
 let initApply = (filesList) => {
     if (filesList) {
-        let files = filesList.querySelectorAll('div.file');
+        let files = filesList.querySelectorAll('.js-diff-progressive-container:first-of-type > div.file');
         if (files) {
             [...files].forEach(file => {
                 applyCommentIcon(file);
@@ -57,11 +61,11 @@ let initApply = (filesList) => {
     }
 };
 
-let filesList = null;
-let observer = new MutationObserver(mutateCallback);
+let pageContent = document.getElementsByTagName('body')[0];
+initApply(document.getElementById('files'));
 
-window.addEventListener('load', (e) => {
-    filesList = document.getElementById('files');
-    initApply(filesList);
-    observer.observe(filesList, { attributes: false, childList: true, subtree: true });
-});
+let observer = new MutationObserver(mutateCallback);
+observer.observe(
+    pageContent,
+    { attributes: false, childList: true, subtree: true }
+);
